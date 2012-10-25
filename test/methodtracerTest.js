a0b81b433732a8a3bb02a178e83b28ed9d19c660
Daniel Bell <daniel.m.bell@gmail.com>
2012-10-25 15:20:34 +1100

    Added further testing.

--- test/methodtracerTest.js
+++ test/methodtracerTest.js
@@ -19,30 +19,70 @@ describe('methodtracer', function() {
   });
 
   describe('#create when env.NODE_METHOD_TRACER_DISABLED is set', function() {
-    var logged = 0, mt;
+    var messages = [], mt;
 
-    before(function() {
+    before(function(done) {
       process.env.NODE_METHOD_TRACER_DISABLED = 1;
-      mt = methodtracer.create({ log: function() { logged += 1; } });
-    });
-    
-    it('should not log anything', function(done) {
+      mt = methodtracer.create({ log: function(message) { messages.push(message); } });
       (function() {
-        var cb = mt.init('testfunction').callback(function(e) {
-          logged.should.equal(0);
-          done(e);
-        });
+        var cb = mt.init('f1').callback(done);
         process.nextTick(cb);
       })();
+      delete process.env.NODE_METHOD_TRACER_DISABLED;
     });
     
-    after(function() {
-      delete process.env.NODE_METHOD_TRACER_DISABLED;
+    it('should not log anything', function() {
+      messages.length.should.eql(0);
     });
   });
 
   describe('success exit', function() {
+    var messages = [], mt;
 
+    before(function(done) {
+      mt = methodtracer.create({ log: function(message) { messages.push(message); } });
+      (function() {
+        var cb = mt.init('f1').callback(done);
+        setTimeout(function() { cb(null, 'the result') }, 100);
+      })();
+    });
+
+    it('should log 2 messages', function() {
+      messages.length.should.eql(2);
+    });
+
+    it('should log an entry message', function() {
+      messages[0].should.eql('>>> f1()');
+    });
+
+    it('should log an exit message', function() {
+      messages[1].should.match(/<<< f1\(\): result=\("the result"\) \([0-9]+ms\)/);
+    });
   });
+
+  describe('error exit', function() {
+    var messages = [], mt;
+
+    before(function(done) {
+      mt = methodtracer.create({ log: function(message) { messages.push(message); } });
+      (function() {
+        var cb = mt.init('f1').callback(function() { done(null); });
+        setTimeout(function() { cb(new Error('the error')) }, 100);
+      })();
+    });
+
+    it('should log 2 messages', function() {
+      messages.length.should.eql(2);
+    });
+
+    it('should log an entry message', function() {
+      messages[0].should.eql('>>> f1()');
+    });
+
+    it('should log an exit message', function() {
+      messages[1].should.match(/<<< f1\(\): error=\(Error: the error\) \([0-9]+ms\)/);
+    });
+  });
+
 
 });
